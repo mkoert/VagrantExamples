09124a2c9187e9883b4c5a6b0dde1ac2d32c257e
Philip Damra <philip@philipdamra.com>
2013-11-16 19:59:31 -0500

    initial commit

---
+++ lamp_stack/provision/modules/email/init.pp
@@ -0,0 +1,14 @@
+class mail {
+	package {"postfix":
+		ensure => present
+	}
+	
+	package {"mailutils":
+		ensure => present
+	}
+	
+	exec {'autostartmail':
+		command => '/usr/sbin/update-rc.d postfix defaults',
+		require => Package['postfix']
+	}
+}
