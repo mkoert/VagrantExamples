efe16297447efc6fd920d544dcb5467d4976291d
Philip Damra <djheru@gmail.com>
2013-11-22 10:17:10 -0500

    tweaks to vhost config

--- provision/modules/apache/manifests/init.pp
+++ provision/modules/apache/manifests/init.pp
@@ -3,9 +3,9 @@ class apache {
 		ensure => present
 	}
 	
-	file { '/var/www/site':
+	file { '/var/www/sites':
 		ensure => 'link',
-		target => '/vagrant/site',
+		target => '/vagrant/sites',
 		require => Package['apache2']
 	}
 	
@@ -21,7 +21,7 @@ class apache {
 		hasrestart => true,
 		subscribe =>[File['/etc/apache2/sites-available/default'],
 			Package['php5', 'php5-cli', 'php5-common', 'php5-pgsql', 'php5-mysql', 'php5-dev', 'php5-curl', 'php5-gd', 'php5-imagick', 
-					'php5-mcrypt', 'php5-memcache', 'php5-mhash', 'php5-pspell', 'php5-snmp', 
+					'php5-mcrypt', 'php5-memcache', 'php5-mhash', 'php5-pspell', 'php5-snmp', 'php5-xdebug',
 					'php5-xmlrpc', 'php5-xsl', 'php-pear', 'libapache2-mod-php5']]		
 	}
 	
